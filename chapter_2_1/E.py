13f8d709f3b7f9ca5f1c6a61e58f3b7b11f758af
Gorbunov Danil <danil45@bk.ru>
2025-03-19 12:54:30 +0500

    Решение задачи E

--- chapter_2_1/E.py
+++ chapter_2_1/E.py
@@ -1,6 +1,6 @@
-price = (input())
-weight = (input())
-banknotes = (input())
+price = int(input())
+weight = int(input())
+banknotes = int(input())
 
 total_price = price * weight
 change = int(banknotes - total_price)
