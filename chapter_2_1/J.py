3036c521df0d823310d845f7505c7252990673ec
Gorbunov Danil <danil45@bk.ru>
2025-03-28 20:44:43 +0500

    решение задач J, K с исправлением

--- chapter_2_1/J.py
+++ chapter_2_1/J.py
@@ -1,9 +1,11 @@
-name = (input())
+name = input()
 wardrobe = int(input())
 
 group_number = wardrobe // 100
 list_number = wardrobe % 10
 bed_number = wardrobe // 10 % 10
 
-print('Группа №' + str(group_number) + '.\n' + str(list_number) + '. ' + name +
-      '.\n' + 'Шкафчик: ' + str(wardrobe) + '.\n' + 'Кроватка: ' + str(bed_number) + '.')
+print(f'Группа №{group_number}.\n'
+      f'{list_number}. {name}.\n'
+      f'Шкафчик: {wardrobe}.\n'
+      f'Кроватка: {bed_number}.')
--- chapter_2_1/K.py
+++ chapter_2_1/K.py
@@ -1,6 +1,8 @@
 number = int(input())
+
 a = number // 100 % 10
 b = number // 1000
 c = number % 10
 d = number // 10 % 10
-print(str(a) + str(b) + str(c) + str(d))
+
+print(f'{a}{b}{c}{d}')
