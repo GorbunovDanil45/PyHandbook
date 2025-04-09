2b8bbe768e162f3fc0f42cdc38c5ef36b112bceb
Gorbunov Danil <danil45@bk.ru>
2025-04-09 21:03:57 +0500

    Решение задачи S

--- chapter_2_1/S.py
+++ chapter_2_1/S.py
@@ -1,4 +1,3 @@
-n = Чек
 name_product = str(input())
 price = int(input())
 weight = int(input())
@@ -7,10 +6,23 @@ money = int(input())
 total_price = price * weight
 change = money - price * weight
 
-print(f'{n:<35}\n'
-      f'Товар: {name_product:>35}\n'
-      f''Цена: '{weight}'кг'' * '{price}'руб/кг: < 35'\n'
-      f'{'Итого:':<35}{total_price}{'руб'}\n'
-      f'{'Внесено:':<35}{money}{'руб'}\n'
-      f'{'Сдача:':<35}{change}{'руб'}\n'
-      f'{'=':=>35}')
+line_1 = 'Чек'
+print(f'{line_1:=^35}')
+
+line_2 = name_product
+print(f'Товар: {name_product:>28}')
+
+line_3 = f'{weight}кг * {price}руб/кг'
+print(f'Цена: {line_3:>29}')
+
+line_4 = f'{total_price}руб'
+print(f'Итого: {line_4:>28}')
+
+line_5 = f'{money}руб'
+print(f'Внесено: {line_5:>26}')
+
+line_6 = f'{change}руб'
+print(f'Сдача: {line_6:>28}')
+
+line_7 = f'='
+print(f'{line_7:=^35}')
