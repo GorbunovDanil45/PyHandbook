5c3431467c989c57f36b622d4adb63273e138348
Gorbunov Danil <danil45@bk.ru>
2025-04-19 22:13:21 +0500

    Решение задачи A

---
+++ chapter_2_2/A.py
@@ -0,0 +1,9 @@
+name = input('Как Вас зовут?')
+print('Здравствуйте,', name, '!')
+
+question = input('Как дела?')
+
+if question == 'хорошо':
+    print('Я рад за Вас!')
+elif question == 'плохо':
+    print('Всё наладится!')
