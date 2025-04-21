1c977901fb329484113782ff8e61d3dbb99d509f
Gorbunov Danil <danil45@bk.ru>
2025-04-21 18:47:24 +0500

    Решение задачи C

---
+++ chapter_2_2/C.py
@@ -0,0 +1,10 @@
+speed_1 = int(input())
+speed_2 = int(input())
+speed_3 = int(input())
+
+if speed_2 < speed_1 > speed_3:
+    print('Петя')
+elif speed_1 < speed_2 > speed_3:
+    print('Вася')
+else:
+    print('Толя')
