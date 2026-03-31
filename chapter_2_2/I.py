ab4d4d71397756fb6b88b75d223384887eff13c0
Gorbunov Danil <danil45@bk.ru>
2026-03-31 16:33:18 +0500

    решение задачи I, параграф 2.2

---
+++ chapter_2_2/I.py
@@ -0,0 +1,10 @@
+name_1 = input()
+name_2 = input()
+name_3 = input()
+
+if name_2 > name_1 < name_3:
+    print(name_1)
+elif name_1 > name_2 < name_3:
+    print(name_2)
+elif name_1 > name_3 < name_2:
+    print(name_3)
