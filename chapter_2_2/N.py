9830ab5fac8ffd9ed9c728f4eb3853eaa0e42343
Gorbunov Danil <danil45@bk.ru>
2026-04-14 02:02:21 +0500

    решение задачи N, раздел 2.2

--- chapter_2_2/N.py
+++ chapter_2_2/N.py
@@ -11,8 +11,8 @@ middle = (hundreds + dozens + units) - high - low
 max_digit = high * 10 + middle
 
 if low == 0:
-    min_digit = mid * 10 + low
+    min_digit = middle * 10 + low
 else:
-    min_digit = low *10 + middle
+    min_digit = low * 10 + middle
 
 print(f'{min_digit} {max_digit}')
