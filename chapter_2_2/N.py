6fecd9c5054ea98e5d4cb0646423e6146d1390fa
Gorbunov Danil <danil45@bk.ru>
2026-04-14 02:01:20 +0500

    решение задачи N, раздел 2.2

---
+++ chapter_2_2/N.py
@@ -0,0 +1,18 @@
+number = int(input())
+
+hundreds = number // 100
+dozens = number // 10 % 10
+units = number % 10
+
+high = max(hundreds, dozens, units)
+low = min(hundreds, dozens, units)
+middle = (hundreds + dozens + units) - high - low
+
+max_digit = high * 10 + middle
+
+if low == 0:
+    min_digit = mid * 10 + low
+else:
+    min_digit = low *10 + middle
+
+print(f'{min_digit} {max_digit}')
