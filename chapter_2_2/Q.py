17a33a5f11ff5fac924e679c5e9207b57aefb617
Gorbunov Danil <danil45@bk.ru>
2026-05-26 16:25:29 +0500

    решение задач Q, S раздел 2.2

--- chapter_2_2/Q.py
+++ chapter_2_2/Q.py
@@ -3,38 +3,29 @@ b = float(input())
 c = float(input())
 
 if a == 0 and b == 0 and c == 0:
-    print('Infinite solutions')
-    
-elif a == 0:
-    if b == 0:
-        print('No solutions')
-    else:
-        x = -c / b
-        if x == 0:
-            x = 0.00
-        print(f'{x:.2f}')
+    print("Infinite solutions")
+elif a == 0 and b != 0 and c != 0:
+    x_1 = -(c / b)
+    print(f"{x_1:.2f}")
+elif a == b == 0:
+    print("No solution")
+elif a == c == 0:
+    print(f"{0:.2f}")
     
 else:
-    D = b ** 2 - 4 * a * c
+    D = (b ** 2) - (4 * a * c)
 
-    if D < 0:
-        print('No solutions')
-
-    elif D == 0:
-        x = -b / (2 * a)
-        if x == 0:
-            x = 0.00
-        print(f'{x:.2f}')
+    if D == 0:
+        x_1 = -b / (2 * a)
+        print(f"{x_1:.2f}")
 
     elif D > 0:
-        root_1 = (-b - D ** 0.5) / (2 * a)
-        root_2 = (-b + D ** 0.5) / (2 * a)
+        root_1 = (-b - (D ** 0.5)) / (2 * a)
+        root_2 = (-b + (D ** 0.5)) / (2 * a)
 
         x_1 = min(root_1, root_2)
         x_2 = max(root_1, root_2)
+        print(f"{x_1:.2f} {x_2:.2f}")
 
-        if x_1 == 0:
-            x_1 = 0.00
-        if x_2 == 0:
-            x_2 = 0.00
-        print(f'{x_1:.2f} {x_2:.2f}')
+    else:
+        print("No solution")
