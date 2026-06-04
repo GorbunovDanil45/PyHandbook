b8e635f54da92e7fe4f9a196a082cd750b722079
Gorbunov Danil <danil45@bk.ru>
2026-06-04 16:09:46 +0500

    глава 2.3, исправление задачи E

--- chapter_2_3/E.py
+++ chapter_2_3/E.py
@@ -1,11 +1,10 @@
 price: float = float(input())
-result: float = 0
+result: float = 0.0
 
 while price != 0:
-    price: float = float(input())
     if price >= 500:
         price = price * 0.9
-    result += price
-    price = float(input())
-    
+    result = price + result
+    price: float = float(input())    
+
 print(result)
