64be4cd14e92f4163d44a5bc1ddd137d16eb2406
Gorbunov Danil <danil45@bk.ru>
2026-06-10 10:26:32 +0500

    Исправление задачи G (добавлен комментарий)

--- chapter_2_3/G.py
+++ chapter_2_3/G.py
@@ -6,6 +6,6 @@ low_num = min(a, b)
 
 while low_num != 0:
     high_num, low_num = low_num, high_num % low_num
-    lcm = (a * b) / high_num 
+    lcm = (a * b) / high_num #lcm - Least Common Multiple
 
 print(int(lcm))
