82b476d89e754dce2bd6fce1a6d7548eaa15c2d2
Gorbunov Danil <danil45@bk.ru>
2026-06-30 14:53:03 +0500

    параграф 2.3, решение задачи T

--- chapter_2_3/T.py
+++ chapter_2_3/T.py
@@ -1,4 +1,22 @@
+prev_h: int = 0
+bad: int = -1
 
+for index in range(int(input())):
+    block: int = int(input())
 
-for _ in range(int(input())):
-    
+    h_n = block % 256
+    r_n = (block // 256) % 256
+    m_n = block // 256 ** 2
+
+    if h_n >= 100:
+        bad = index
+        break
+
+    expected_h = (37 * (m_n + r_n + prev_h)) % 256
+
+    if h_n != expected_h:
+        bad = index
+        break
+    prev_h = h_n
+
+print(bad)
