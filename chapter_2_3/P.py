f6cadc7656bf730f17a1452c95e26557d6f3787d
Gorbunov Danil <danil45@bk.ru>
2026-06-30 16:53:16 +0500

    параграф 2.3, решение задачи P

---
+++ chapter_2_3/P.py
@@ -0,0 +1,15 @@
+number: int = int((int(input()) ** 2) ** 0.5)
+
+temp_number = number
+revers_number: int = 0
+
+while number != 0:
+    last_digit = number % 10
+    revers_number = revers_number * 10 + last_digit
+    number = number // 10
+
+if revers_number == temp_number:
+    print('YES')
+
+else:
+    print('NO')
